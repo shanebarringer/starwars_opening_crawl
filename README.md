5ccc1b9149a3aac2b0c28c53baf99d0d8b464796
Shane Barringer <shane.barringer@outlook.com>
2016-12-06 23:50:01 -0500

    adds initial layout and fetch request for episodes by name and episode_id

---
+++ README.md
@@ -0,0 +1,4 @@
+Basic little react app that searches the [StarWars API](https://swapi.co/) and displays the opening crawl
+
+Styles influenced by [Pontus Lundin
+](https://github.com/hontas)'s [gist](https://gist.github.com/hontas/3955052)
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      filmName: '',
+      film: ''
+    }
+  }
+
+  setFilmName = (e) => {
+    this.setState({filmName: e.target.value})
+  }
+
+  findFilm = (e) => {
+    e.preventDefault()
+    if (this.state.filmName.length > 1) {
+      fetch(`http://swapi.co/api/films/?search=${this.state.filmName}`)
+      .then((res) => res.json())
+      .then((film) => this.setState({film: film.results[0]}))
+    } else {
+      fetch(`http://swapi.co/api/films/`)
+      .then((res) => res.json())
+      .then((data) => {
+        let film = data.results.find((result) => result.episode_id === parseInt(this.state.filmName))
+        this.setState({film})
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='search'>
+          { !this.state.film ?
+            <form onSubmit={this.findFilm}>
+              <h3> Search for a Film by Name of Episode Number</h3>
+              <input type='text' onChange={this.setFilmName}/>
+            </form>
+            : <h3>{this.state.film.title}</h3>
+          }
+        </div>
+        <div className="title">
+          <div className="titlecontent">
+            <p>{this.state.film.opening_crawl}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
